996bda8978d3b501f414111c982b3cbaaa642a16
Dubjay18 <jejeniyi7@gmail.com>
2023-09-12 21:03:28 +0100

    handled routes that don't exist

--- src/app.js
+++ src/app.js
@@ -10,4 +10,21 @@ const app = express();
 app.use(express.json());
 app.use("/api", PersonRouter);
 
+// Middleware function to handle routes that don't exist
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// Error handling middleware function
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
 module.exports = app;
